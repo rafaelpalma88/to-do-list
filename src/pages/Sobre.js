b6e8e9dc15441bc92c64bafa3012e319d83c9697
Rafael Costa Palma <palma3482@gmail.com>
2019-10-23 16:42:55 -0300

    creating component tasks

---
+++ src/pages/Sobre.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Bootstrap.css';
+import '../App.css';
+
+function Sobre() {
+  return (
+    <div>    
+        <main role="main">     
+            <div className="jumbotron">
+                <div className="container">
+                    <h1 className="display-3">Sobre</h1>
+                    <p>Nossa empresa é muito idônea!</p>                
+                </div>
+            </div>            
+        </main>        
+    </div>
+    )
+}
+
+export default Sobre;
